48f2170d01190d017a37c41ecf2c9607442eab42
Rafael Fourquet <fourquet.rafael@gmail.com>
2018-08-30 14:57:21 +0200

    add bits

--- src/Bits.jl
+++ src/Bits.jl
@@ -1,6 +1,8 @@
+# * Bits
+
 module Bits
 
-export bitsize
+export bitsize, bits
 
 using Base: BitIntegerType
 
@@ -39,5 +41,68 @@ bitsize(::Type{BigInt}) = INF
 bitsize(::Type{Bool}) = 1
 bitsize(T::Type) = throw(MethodError(bitsize, (T,)))
 bitsize(x) = bitsize(typeof(x))
+
+lastactualpos(x::Integer) = bitsize(x)
+lastactualpos(x::BigInt) = abs(x.size) * sizeof(Base.GMP.Limb) * 8
+
+
+# * bits & BitVector1
+
+"""
+    bits(x::Integer)
+
+Create an immutable view on the bits of `x` as a vector of `Bool`, similar to a `BitVector`.
+If `x` is a `BigInt`, the vector has length [`Bits.INF`](@ref).
+Currently, no bounds check is performed when indexing into the vector.
+
+# Examples
+```jldoctest
+julia> v = bits(Int16(2^8+2^4+2+1))
+|00000001 00010011|
+
+julia> permutedims([v[i] for i in 8:-1:1])
+1×8 Array{Bool,2}:
+ false  false  false  true  false  false  true  true
+
+julia> bits(true)
+|1|
+
+julia> bits(big(2)^63)
+|...0 10000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000|
+```
+"""
+bits(x::Integer) = BitVector1(x)
+
+
+# ** BitVector1
+
+# similar to a BitVector, but with only 1 word to store bits (instead of 1 array thereof)
+struct BitVector1{T<:Integer} <: AbstractVector{Bool}
+    x::T
+end
+
+Base.size(v::BitVector1) = (bitsize(v.x),)
+Base.getindex(v::BitVector1, i::Integer) = (v.x >> (UInt(i)-1)) % Bool
+
+
+# ** show
+
+function Base.show(io::IO, v::BitVector1)
+    if v.x isa Bool
+        return print(io, "|$(Int(v[1]))|")
+    elseif v.x isa BigInt
+        print(io, "|...", v.x < 0 ? "1 " : "0 ")
+    else
+        print(io, "|")
+    end
+    for i = lastactualpos(v.x):-1:1
+        show(io, v[i] % Int)
+        i % 8 == 1 && i != 1 && print(io, ' ')
+    end
+    print(io, "|")
+end
+
+Base.show(io::IO, ::MIME"text/plain", v::BitVector1) = show(io, v)
+
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -10,3 +10,24 @@ using Bits , Test
     @test bitsize(Bool) === 1
     @test_throws MethodError bitsize(Float64)
 end
+
+@testset "bits" begin
+    for T in Base.BitInteger_types
+        v = bits(one(T))
+        @test length(v) == bitsize(T)
+        @test v[1] === true
+        @test count(v) == 1
+        v = bits(zero(T))
+        @test length(v) == bitsize(T)
+        @test v[1] === false
+        @test count(v) == 0
+    end
+    v = bits(7)
+    @test v[1] === v[2] === v[3] === true
+    @test count(v) == 3
+    for T = (Int64, UInt64)
+        v = bits(T(2)^63)
+        @test v[64] === true
+        @test count(v) == 1
+    end
+end
